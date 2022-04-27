08f6569cc03fe165aafbdc18a34937c3f4342687
KASHI TAMANG <tamang.kashi@gmail.com>
2022-04-27 11:58:07 -0700

    addded button event listener and internal function to pick random item and add to picnic

--- app.js
+++ app.js
@@ -5,26 +5,52 @@ const grapes = document.getElementById('grapes');
 const bread = document.getElementById('bread');
 const watermelon = document.getElementById('watermelon');
 
-console.log(soda, cheese, grapes, bread, watermelon);
+const button = document.getElementById('button');
+
 
 //add event listeners for clicking on each elements
 
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
 });
+//commit
 
 cheese.addEventListener('click', () => {
     cheese.classList.toggle('picked');
 });
+//commit
 
 grapes.addEventListener('click', () => {
     grapes.classList.toggle('picked');
 });
+//commit
 
 bread.addEventListener('click', () => {
     bread.classList.toggle('picked');
 });
+//commit
 
 watermelon.addEventListener('click', () => {
     watermelon.classList.toggle('picked');
 });
+
+//Add a button that will pick a random item from your picnic basket
+//and add it to your picnic
+
+button.addEventListener('click', () => {
+    const pickRandomItem = Math.floor(Math.random() * 5);
+    console.log(pickRandomItem);
+    
+    if (pickRandomItem === 1){
+        soda.classList.toggle('picked');
+    } else if (pickRandomItem === 2){
+        cheese.classList.toggle('picked');
+    } else if (pickRandomItem === 3){
+        grapes.classList.toggle('picked');
+    } else if (pickRandomItem === 4){
+        bread.classList.toggle('picked');
+    } else {
+        watermelon.classList.toggle('picked');
+    }
+});
+
